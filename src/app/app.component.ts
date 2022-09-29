d7d5ebacf6ffc26efbbf62fc11c7d616cd67d16f
Sadowski Krystian <krystek.sadowski@gmail.com>
2022-09-29 11:54:38 +0200

    [mod] 29.09.2022

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'App';
+  myAppName = "<i>edu</i>"
+  constructor(){
+    console.log("construct");
+    
+  }
+
+  @HostListener('window:load')
+  onLoad(){
+    console.log('loads');
+    let p = document.querySelector('p')
+    console.log(p);
+    // p.textContent = "<i>edu</i>"
+    
+    
+  }
+
+  ngOnIniy(){
+    console.log("init");
+    
+  }
+
+  mojaMetoda (){
+    console.log("cześć")
+  }
 }
